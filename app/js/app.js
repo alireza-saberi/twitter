e413f632ead1bb55eb96b846950bddd01b77a75b
alireza-saberi <saberi.ar.eng@gmail.com>
2016-09-30 01:29:16 -0400

    feat: twitter factory is initialized
    without talking to twitter api, twitter factory is mockup

--- app/js/app.js
+++ app/js/app.js
@@ -2,8 +2,23 @@
 // Container module
 var app = angular.module('twitterApp', []);
 
+// factories
+app.factory("twitter", ["$http", function( $http ) {
+    var twts = {};
+    twts.getTweets = function( searchString ){
+        // return $http.get('/condidateslist');
+        return [
+            {message: 'tweet1', img:'img1'}, 
+            {message: 'tweet2', img:'img2'},
+            {message: 'tweet3', img:'img3'}, 
+            {message: 'tweet4', img:'img4'}
+        ];
+    }
+    return twts;
+}]);
+
 // Controllers
-app.controller('twitterController', ["$scope", function ($scope) {
+app.controller('twitterController', ["$scope", "twitter", function ( $scope, twitter ) {
     $scope.hideTable = true;
     $scope.searchbox = {
     	str: "",
@@ -11,15 +26,9 @@ app.controller('twitterController', ["$scope", function ($scope) {
     }
     $scope.tweets = new Array(); 
 
-    $scope.search = function( searchString) {
+    $scope.search = function( searchString ) {
     	$scope.hideTable = false;
-    	$scope.tweets = [
-            {message: 'tweet1', img:'img1'}, 
-            {message: 'tweet2', img:'img2'},
-            {message: 'tweet3', img:'img3'}, 
-            {message: 'tweet4', img:'img4'}
-        ];
+        $scope.tweets = twitter.getTweets( searchString );
     };
 }]);
 
-// facotried
